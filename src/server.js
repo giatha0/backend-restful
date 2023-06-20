42dbe0662677c8762c85e223bd431c1d06dbd750
giatha0 <thao8c89@gmail.com>
2023-06-21 00:23:26 +0700

    config routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,14 +10,8 @@ const hostname = process.env.HOST_NAME || 'localhost'
 // config template engine
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/thao', (req, res) => {
-    res.render('sample')
-})
-
+// config routes
+app.use('/', webRoutes);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)

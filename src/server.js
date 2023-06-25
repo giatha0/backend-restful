0921d522bc673b6b0cc078e9268d8b29f9812828
giatha0 <thao8c89@gmail.com>
2023-06-25 17:19:51 +0700

    connect voi database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
+const mysql = require('mysql2');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,6 +13,24 @@ configViewEngine(app);
 
 // config routes
 app.use('/', webRoutes);
+
+// test connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'giathao'
+});
+
+
+connection.query(
+    'SELECT * FROM Users ',
+    function (err, results, fields) {
+        console.log('results', results); // results contains rows returned by server
+        console.log('fields', fields); // fields contains extra meta data about results, if available
+    }
+);
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)

cedbfc50ac0ba4cd03e4cda4fd6332af228e793b
giatha0 <thao8c89@gmail.com>
2023-07-10 17:27:29 +0700

    config express-fileupload

--- src/server.js
+++ src/server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
+const fileUpload = require('express-fileupload');
 
 const connection = require('./config/database');
 
 const app = express();
 const port = process.env.PORT || 8080;
 const hostname = process.env.HOST_NAME || 'localhost'
+
+// config file upload
+app.use(fileUpload());
 
 // config req.body
 app.use(express.json()); // for parsing application/json

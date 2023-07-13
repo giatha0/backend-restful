687f0bc5dc84124e758e3edaddfdafcbcfdd94ae
giatha0 <thao8c89@gmail.com>
2023-07-13 17:52:39 +0700

    su dung mongodb driver

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const apiRoutes = require('./routes/api');
 const fileUpload = require('express-fileupload');
+const { MongoClient } = require('mongodb');
 
 const connection = require('./config/database');
 
@@ -30,7 +31,33 @@ app.use('/v1/api/', apiRoutes);
 
 (async () => {
     try {
-        await connection();
+        // using mongoose
+        // await connection();
+
+
+        // using mongodb driver
+        // connection URL
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('customers');
+
+
+
+        // collection.insertOne({
+        //     name: 'test',
+        //     address: [1, 2]
+        // });
+        // collection.insertOne({ address: 'hanoi' });
+        // collection.insertOne({ number: [1, 2, 3] })
+
+        let rs = await collection.find({}).toArray();
+        console.log('rs', rs);
+
         app.listen(port, hostname, () => {
             console.log(`Example app listening on port ${port}`)
         })

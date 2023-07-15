f4a8b2ed3dbce32f884f3fd17729200cb18b4fcb
giatha0 <thao8c89@gmail.com>
2023-07-15 20:58:22 +0700

    remove child in parent with pull and pullall

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -5,10 +5,11 @@ const postCreateProject = async (req, res) => {
     const data = req.body;
     // console.log('data', data);
     const project = await projectService.createProject(data);
-    console.log('project', project);
+    // console.log('project', project);
     return res.status(200).json({
         message: 'create project successfully',
-        data: project
+        data: project,
+        // t: project.t
     });
 }
 
--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,30 +1,35 @@
 const Project = require('../models/project.js');
+const User = require('../models/user.js');
 
 const createProject = async (data) => {
-    console.log('data', data);
+    console.log('data', data.userArr);
     let myProject = null;
+
     if (data.type === "EMPTY PROJECT") {
         myProject = await Project.create(data);
-        console.log('myProject', myProject);
+
         // return myProject;
     }
 
     if (data.type === 'ADD-USER') {
-        // myProject = await Project.findOneAndUpdate(
-        //     { _id: data.projectId },
-        //     { $push: { userInfor: data.userArr } },
-        //     { new: true }
-        // );
-        // console.log('myProject', myProject);
-
-        myProject = await Project.findOne({ _id: data.projectId });
-        console.log('myProject', myProject);
-        myProject.userInfor.push(data.userArr);
-        await myProject.save();
-
-
+        myProject = await Project.findOneAndUpdate(
+            { _id: data.projectId },
+            { $push: { userInfor: data.userArr } },
+            { new: true }
+        );
     }
 
+    if (data.type === "REMOVE-USER") {
+
+        myProject = await Project.findOneAndUpdate(
+            { _id: data.projectId },
+            { $pullAll: { userInfor: data.userArr } },
+            { new: true },
+
+        );
+    }
+
+    // console.log('myProject', myProject);
     return myProject;
 }
 
